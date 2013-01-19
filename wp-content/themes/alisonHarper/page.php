dcfb4a9db66350ff4d4ccbec89ab54aa5ae2683b
Jocelyn Casto <jlcasto@gmail.com>
2013-01-18 22:36:22 -0500

    test work

--- wp-content/themes/alisonHarper/page.php
+++ wp-content/themes/alisonHarper/page.php
@@ -5,5 +5,11 @@
     <?php endwhile; else: ?> 
         <p><?php _e('Sorry, this page does not exist.'); ?></p> 
     <?php endif; ?>
+<?php 
+$page_id = 68;
+
+$page_data = get_page( $page_id );
+
+echo apply_filters('the_content', $page_data->post_content); ?>
 </div>
 <?php get_footer(); ?>
