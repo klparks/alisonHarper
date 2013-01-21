4f7959ae13140d4321afe101e1cb95acd2ce4ab7
Jocelyn Casto <jlcasto@gmail.com>
2013-01-21 17:37:38 -0500

    Home Page Template

---
+++ wp-content/themes/alisonHarper/page-templates/alisonHarperHome.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
+<head>
+  <meta charset="utf-8">
+  <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Le styles -->
+  <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+
+  <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <?php wp_enqueue_script("jquery"); ?>
+  <?php wp_head(); ?>
+</head>
+<body>
+<div class="container">
+This is the home page
+<?php get_footer(); ?>
