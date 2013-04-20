80fe1c35cff7cf4e3a0c44326c8e813be888c705
Jocelyn Casto <jlcasto@gmail.com>
2013-04-20 12:36:46 -0400

    Adding a page back that i shouldn't have deleted...

---
+++ wp-content/themes/alisonHarper/page-templates/splash.php
@@ -0,0 +1,68 @@
+<?php
+/*
+  Template Name: Splash Page
+ */
+?>
+<head>
+    <meta charset="utf-8">
+    <title><?php wp_title('|', 1, 'right'); ?> <?php bloginfo('name'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le styles -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,500,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+
+    <!--[if gte IE 9]>
+  <style type="text/css">
+    .gradient {
+       filter: none;
+    }
+  </style>
+<![endif]-->
+
+    <?php wp_enqueue_scripts(); ?>
+</head>
+<body class="landingBody gradient">
+    <nav class="topHomeNav uppercase shadow"><ul>
+            <li><a href="<?php echo get_page_link(BLOG_PAGE_ID); ?>">Blog</a></li>
+            <li><a href="<?php echo get_page_link(CONTACT_PAGE_ID); ?>">Contact</a></li>
+        </ul></nav>
+
+
+    <div class="container">
+        <header>
+
+            <hgroup>
+                <h1 id="logo"><img src="/wp-content/themes/alisonHarper/images/logo.jpg" alt="Alison Harper and Company"/><span>Alison Harper and Company</span></h1>
+                <h2 id="tagline" class="lowercase"> <?php echo get_bloginfo('description'); ?></h2>
+            </hgroup>
+            <aside>
+                <p class="lowercase headline ">Hello Friend!</p>
+                <p class="lowercase subline">Please select your closest location.</p>
+            </aside>
+        </header>
+
+
+    </div>
+
+    <nav class="locationsList uppercase"><ul>
+            <?php
+            $regions = getLocations();
+            foreach ($regions as $r) {
+                echo '<li><h3>' . $r->name . '</h3></li>';
+                foreach ($r->cities as $city) {
+                    echo '<li><a href="' . $city->url . '">' . $city->name . '</a></li>';
+                }
+            }
+            ?>
+        </ul></nav>
+
+    <?php get_footer(); ?>
+</body>
