5df7d0504deac968fb2f556f3dfcf4f44ecd133b
Jocelyn Casto <jlcasto@gmail.com>
2013-05-21 11:13:45 -0400

    Adding social media links to homepage

--- wp-content/themes/alisonHarper/page-templates/splash.php
+++ wp-content/themes/alisonHarper/page-templates/splash.php
@@ -5,15 +5,17 @@
 ?>
 <?php get_template_part("page-templates/header", "head"); ?>
 <body class="landingBody gradient">
-    <nav class="topHomeNav uppercase shadow"><ul>
+    <nav class="topHomeNav uppercase shadow">
+        <ul>
+            <li><?php get_template_part("page-templates/header", "socialMedia"); ?></li>
             <li><a href="<?php echo get_page_link(BLOG_PAGE_ID); ?>">Blog</a></li>
             <li><a href="<?php echo get_page_link(CONTACT_PAGE_ID); ?>">Contact</a></li>
-        </ul></nav>
+        </ul>
+    </nav>
 
 
     <div class="container">
         <header>
-
             <hgroup>
                 <h1 id="logo"><img src="/wp-content/themes/alisonHarper/images/logo.jpg" alt="Alison Harper and Company"/><span>Alison Harper and Company</span></h1>
                 <h2 id="tagline" class="lowercase"> <?php echo get_bloginfo('description'); ?></h2>
@@ -23,8 +25,6 @@
                 <p class="lowercase subline">Please select your closest location.</p>
             </aside>
         </header>
-
-
     </div>
 
     <nav class="locationsList uppercase"><ul>
@@ -37,7 +37,7 @@
                 }
             }
             ?>
-        </ul></nav>
-
+        </ul>
+    </nav>
     <?php get_footer(); ?>
 </body>
