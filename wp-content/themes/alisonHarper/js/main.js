24e5763dc3a45a13bec96977f56b18bbe6897051
Jocelyn Casto <jlcasto@gmail.com>
2013-06-01 15:58:04 -0400

    jQuery love for ancestor highlighting

--- wp-content/themes/alisonHarper/js/main.js
+++ wp-content/themes/alisonHarper/js/main.js
@@ -3,4 +3,12 @@ jQuery(document).ready(function($) {
     $("select").customSelect();
     $("input.date").datepicker();
     $("input[type=submit], button" ).button();
+    if(!$("aside.left .current_page_item")){
+    var ancestors = $(".headerContents nav .current_page_ancestor");
+    $.each(ancestors, function(idx, e){
+        if(!$(e).hasClass("current_page_parent")){
+            $(e).addClass("noHighlight");
+        }
+    });
+}
 });
