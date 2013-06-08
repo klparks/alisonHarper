c4d429ceb848e19e97f0399e93ac9af92514cab1
Kristen Parks <kristen.parks@elevenpeppers.com>
2013-06-08 17:55:47 -0400

    more styles

--- wp-content/themes/alisonHarper/index.php
+++ wp-content/themes/alisonHarper/index.php
@@ -1,2 +1,21 @@
 <!-- This is our 404 not found page -->
-404 - Page not found
+<div class="intro"><span class="headline">Hello Friend.</span>
+<p>It's time for your close up! Come back to Alison Harper and Company.</p></div>
+<ul>
+            <li class="locationSelect"><select name="page-dropdown"
+                        onchange='if(this.options[this.selectedIndex].value != "")document.location.href=this.options[this.selectedIndex].value;'> 
+                    <option value=""><?php echo esc_attr(__('Select location')); ?></option> 
+                    <?php
+                    $regions = getLocations();
+                    foreach ($regions as $r) {
+                        echo '<option value="" class="disabled" disabled="disabled">' . esc_attr(__($r->name)) . '</option> ';
+                        foreach ($r->cities as $l) {
+                            $option = '<option class="indent" value="' . $l->url . '">';
+                            $option .= $l->name;
+                            $option .= '</option>';
+                            echo $option;
+                        }
+                    }
+                    ?>
+                </select></li>
+                </ul>
