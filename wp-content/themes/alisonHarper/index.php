d8526cbab23a41102bbc409335657fe5cb7519cf
Kristen Parks <kristen.parks@elevenpeppers.com>
2013-06-09 00:59:21 -0400

    404 page styles

--- wp-content/themes/alisonHarper/index.php
+++ wp-content/themes/alisonHarper/index.php
@@ -1,7 +1,7 @@
 <!-- This is our 404 not found page -->
 <?php get_header("noNav"); ?>
-<div class="intro"><span class="headline">Hello Friend.</span>
+<div class="intro error clear"><span class="headline">Hello Friend.</span>
 <p>It's time for your close up! Come back to Alison Harper and Company.</p></div>
-<ul>
+<ul class="error">
    <li class="locationSelect"><?php get_template_part("page-templates/footer", "select"); ?></li>
 </ul>
