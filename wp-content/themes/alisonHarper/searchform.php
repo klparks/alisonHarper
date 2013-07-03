3cec3d10495a0b0e9a4736a4eb34d5506bfba79d
Kristen Parks <kristen.parks@elevenpeppers.com>
2013-07-02 23:56:23 -0400

    css I forgot to commit, doh!

--- wp-content/themes/alisonHarper/searchform.php
+++ wp-content/themes/alisonHarper/searchform.php
@@ -1,5 +1,5 @@
 <form method="get" id="searchform" action="<?php bloginfo('siteurl'); ?>/">
 	<div>
-		<input type="text" size="put_a_size_here" name="s" id="s" value="Search" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
+		<input type="text" size="put_a_size_here" name="s" id="s" value="Search our Blog" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
 	</div>
 </form>
