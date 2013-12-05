f392bbd619443de60644a7799862b98a3460829a
Kristen Parks <kristen.parks@elevenpeppers.com>
2013-12-05 00:58:52 -0500

    no change to this file
    I tested with the "root" password but reverted changes. Git is making
    me commit anyway.

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'alisonharper');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'c00lR00t!');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
