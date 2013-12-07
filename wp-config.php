258a22aef6fb50f104c047b9d3b859985e28a434
Josie <jlcasto@gmail.com>
2013-12-06 19:00:49 -0500

    more git changes

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'alisonharper');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'c00lr00t!');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
