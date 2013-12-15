764340d6ab1cc787635c8693567a5df34295f8ea
Josie <jlcasto@gmail.com>
2013-12-15 16:39:42 -0500

    removing a line

--- wp-content/themes/alisonHarper/page-templates/page-cityHome.php
+++ wp-content/themes/alisonHarper/page-templates/page-cityHome.php
@@ -16,8 +16,6 @@
     </section>
 	
     <section class="featuredLinks clearfix">
-        
-        <!-- <?php echo gcb(34); ?> -->
         <div class="left"><a href="about/alison"><img src="http://alisonHarper.dev/wp-content/uploads/2013/11/meetAlisonHarper.png" alt="meetAlisonHarper" width="369" height="215" class="alignleft size-full wp-image-1404" /></a></a></div>
         <div class="center left"><a href="services"><a href="<?php echo get_page_link(getLocationPage(SERVICES_PAGE_SLUG)->ID) ?>"><img src="http://alisonHarper.dev/wp-content/uploads/2013/11/reviewOurServices.png" alt="reviewOurServices" width="235" height="211" class="alignleft size-full wp-image-1406" /></a></a></div>
         <div class="left"><a href="/blog/"><a href="/blog/"><img src="http://alisonHarper.dev/wp-content/uploads/2013/11/readTheBlog.png" alt="readTheBlog" width="292" height="204" class="alignleft size-full wp-image-1405" /></a></a></div>
