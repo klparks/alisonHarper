968372127ed8a1df7af1f1c28b9d630be633a17a
Josie <jlcasto@gmail.com>
2013-12-15 17:27:30 -0500

    adding header div

--- wp-content/themes/alisonHarper/page-templates/page-team.php
+++ wp-content/themes/alisonHarper/page-templates/page-team.php
@@ -1,6 +1,6 @@
 <div class="content left">
     <section class="featuredText">
-    <h2>Meet the Team</h2>
+        <header><h2>Meet the Team</h2></header>
         <?php listTeamMembers(); ?>
     </section>
 </div>
