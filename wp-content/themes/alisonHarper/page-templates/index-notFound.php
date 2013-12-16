012af45946c13efcb4d190cb5eba241a15e0c231
Kristen Parks <kristen.parks@elevenpeppers.com>
2013-12-15 20:40:27 -0500

    fixed collapse/expand styles

--- wp-content/themes/alisonHarper/page-templates/index-notFound.php
+++ wp-content/themes/alisonHarper/page-templates/index-notFound.php
@@ -1,4 +1,11 @@
-<p>-404 - Page not found</p>
-<div class="intro error clear"><span class="headline">Hello Friend.</span>
-<p>It's time for your close up! Come back to Alison Harper and Company.</p>
+<aside class="uppercase left">
+           &nbsp; 
+</aside>
+        
+<div class="content left">
+	<p>-404 - Page not found</p>
+  <div class="intro error clear"><span class="headline">Hello Friend.</span>
+			<p>It's time for your close up! Come back to <a href="http://www.alisonharperandcompany.com">Alison Harper and Company.</a></p>
+  </div>
+
 </div>
